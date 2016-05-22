5513f4c9b6de0ac98e38a6588cbfee0bc4e61160
Myst <tormyst@github.com>
2016-05-22 18:50:10 -0400

    Added 2 commands to customize when a game starts
    added playing and removeplaying command.

--- bot/clientSetup.py
+++ bot/clientSetup.py
@@ -1,5 +1,7 @@
 import discord
+from shlex import split
 from .preferences import Preferences
+
 
 def handle_write(c:discord.Client, message:discord.Message, command:list, pref:Preferences):
     if message.server and len(command) == 3:
@@ -12,19 +14,49 @@ def handle_write(c:discord.Client, message:discord.Message, command:list, pref:P
             yield from c.send_message(message.channel, 'Something went wrong while saving the setting.')
         yield from c.send_message(message.channel, 'Ok, I will use {}'.format(toSet))
 
+
+def handle_playing(c:discord.Client, message:discord.Message, command:list, pref:Preferences):
+    if len(command) == 4:
+        pref.addGameSaying(command[2], command[3])
+        yield from c.send_message(message.channel, 'Done.')
+    else:
+        yield from c.send_message(message.channel, 'I don\'t get what you are telling me to do.  '
+                                                   'Make sure to use quotation marks around the game name'
+                                                   ' and the saying')
+
+
+def handle_removeplaying(c: discord.Client, message: discord.Message, command: list, pref: Preferences):
+    if len(command) == 4:
+        if pref.rmGameSaying(command[2], command[3]):
+            yield from c.send_message(message.channel, 'Removed.')
+        else:
+            yield from c.send_message(message.channel, 'Could not remove that.  Check that you wrote it right.')
+    else:
+        yield from c.send_message(message.channel, 'I don\'t get what you are telling me to do.  '
+                                                   'Make sure to use quotation marks around the game name'
+                                                   ' and the saying')
+
+
 def handle_help(c:discord.Client, message:discord.Message, command:list, pref:Preferences):
     help_text = "All commands start with a mention of me, " \
                "the same way you talk to people who are as far as know not bots.\n" \
                "Commands:\n" \
-               "\t➢ `write [here|#channelname]`\n" \
+               "\t➢ `write [here|#<channelname>]`\n" \
                "\t\tsets which channel I write to.\n" \
-               "\t➢ `about`\n" \
+               "\t➢ `playing \"<game>\" \"<is playing text>\"`\n" \
+               "\t\tReplaces \"is now playing\" with is playing text for a specific game.  " \
+                "Randomly will select from all options given for that game. \n" \
+                "\t➢ `removeplaying \"<game>\" \"<is playing text>\"`\n" \
+                "\t\tRemoves the is playing text for that game indicated.\n" \
+                "\t➢ `about`\n" \
                "\t\tPrints what I do."
     yield from c.send_message(message.channel, help_text)
+
 
 def handle_about(c:discord.Client, message:discord.Message, command:list, pref:Preferences):
     aboutText = "Hello, I am a bot that will post a message whenever I detect a player starting up a game."
     yield from c.send_message(message.channel, aboutText)
+
 
 def clientSetup(pref:Preferences):
     c = discord.Client()
@@ -32,12 +64,17 @@ def clientSetup(pref:Preferences):
     @c.async_event
     def on_message(message:discord.Message):
         if message.content.startswith(c.user.mention) and not message.author.bot:
-            command = message.content.lower().split(" ")
-            if command[1] == "write":
+            command = split(message.content)
+            commandWord = command[1].lower()
+            if commandWord == "write":
                 yield from handle_write(c, message, command, pref)
-            elif command[1] == "help":
+            elif commandWord == "playing":
+                yield from handle_playing(c, message, command, pref)
+            elif commandWord == "removeplaying":
+                yield from handle_removeplaying(c, message, command, pref)
+            elif commandWord == "help":
                 yield from handle_help(c, message, command, pref)
-            elif command[1] == "about":
+            elif commandWord == "about":
                 yield from handle_about(c, message, command, pref)
             else:
                 yield from c.send_message(message.channel, "I don't understand what you said.  Try asking for help with {} help".format(c.user.mention))
@@ -46,6 +83,6 @@ def clientSetup(pref:Preferences):
     def on_member_update(old:discord.Member, new:discord.Member):
         if new.game and old.game != new.game and not old.bot:
             sendOn = pref.getServerChannel(old.server)
-            yield from c.send_message(sendOn, "{} is now playing {}.".format(new.name, new.game.name))
+            yield from c.send_message(sendOn, "{0} {1}.".format(new.name, pref.getGameSaying(new.game.name)))
 
     return c
--- bot/preferences.py
+++ bot/preferences.py
@@ -1,8 +1,10 @@
 import os
 import pickle
+from random import choice
 from discord import Server, Channel
 
 channelFile = "data/channelPref.dat"
+gameFile = "data/gamePref.dat"
 
 class Preferences:
     def __init__(self):
@@ -13,8 +15,15 @@ class Preferences:
         else:
             self.serverChannels = {}
 
+        if os.path.exists(gameFile):
+            f = open(gameFile, 'rb')
+            self.gameSayings = pickle.load(f)
+            f.close()
+        else:
+            self.gameSayings = {}
+
     def getServerChannel(self, s:Server) -> Channel:
-        if s.id in self.serverChannels:
+        if s.id in self.serverChannels.keys():
             return s.get_channel(self.serverChannels[s.id])
         else:
             return s.default_channel
@@ -31,3 +40,42 @@ class Preferences:
             f.close()
         return retVal
 
+    def getGameSaying(self, g):
+        if g in self.gameSayings.keys() and len(self.gameSayings[g]) > 0:
+            return choice(self.gameSayings[g])
+        else:
+            return "is now playing {}".format(g)
+
+    def addGameSaying(self, g, saying):
+        if g in self.gameSayings:
+            self.gameSayings[g].append(saying)
+        else:
+            self.gameSayings[g] = [saying]
+        retVal = True
+        try:
+            f = open(gameFile, 'wb+')
+            pickle.dump(self.gameSayings, f)
+        except IOError:
+            retVal = False
+        else:
+            f.close()
+        return retVal
+
+    def rmGameSaying(self, g, saying):
+
+        if g in self.gameSayings:
+            try:
+                self.gameSayings[g].remove(saying)
+            except ValueError:
+                return False
+            retVal = True
+            try:
+                f = open(gameFile, 'wb+')
+                pickle.dump(self.gameSayings, f)
+            except IOError:
+                retVal = False
+            else:
+                f.close()
+            return retVal
+        else:
+            return False
